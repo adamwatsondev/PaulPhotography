8193d705a69b829d530cf2a70d11f1da25c56189
awatson7777 <awatson7777@gmail.com>
2024-12-23 08:03:39 +0000

    major update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Gallery from './pages/gallery';
 import Contact from './pages/contact';
+import GalleryPage from './pages/galleryPage'; // Import the GalleryPage component
 
 function App() {
 
   return (
     <div className="w-full h-screen p-4">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/gallery/:id" element={<GalleryPage />} /> {/* Add dynamic gallery route */}
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
-      </div>
-  )
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
