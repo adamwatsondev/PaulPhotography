7371eabe11e6ff53637aa7851c9e364cf18f760a
awatson7777 <awatson7777@gmail.com>
2025-01-03 03:04:52 +0000

    footer changes

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,22 +1,33 @@
-import { Img } from "react-image"
-import { Link } from "react-router-dom"
+import { Img } from "react-image";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <>
-      <div className="flex w-full justify-between px-4 sm:px-16 h-12 items-center bg-white shadow-md">
-       <div className="flex items-center gap-4 justify-center">
-
-        <Link to="https://www.instagram.com/cahill_paul/?hl=en" className="text-black bg-white focus:outline-none"> 
-          <Img className="h-4 w-4" src="../images/instagram-icon.png" alt="Instagram" />
-        </Link>
-        <span className="text-black text-sm items-center font-old-standard">
-          Copyright Paul Cahill
-          </span>
+      <div className="flex w-full justify-center px-16 h-16 items-center bg-white shadow-md">
+        <div className="flex items-center gap-4 justify-center">
+          <Link
+            to="/contact"
+            className="text-black bg-white focus:outline-none"
+          >
+            <Img
+              className="h-9 w-9"
+              src="../images/Assets/mail.jpg"
+              alt="Mail"
+            />
+          </Link>
+          <Link
+            to="https://www.instagram.com/cahill_paul/?hl=en"
+            className="text-black bg-white focus:outline-none"
+          >
+            <Img
+              className="h-6 w-6"
+              src="../images/Assets/instagram-icon.png"
+              alt="Instagram"
+            />
+          </Link>
         </div>
       </div>
-
-      </>
-  )
+    </>
+  );
 }
-
--- src/pages/galleries.tsx
+++ src/pages/galleries.tsx
@@ -1,30 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
 
 export default function Galleries() {
-
   const imagesArray = [
     {
-      src: "..\\images\\brighton\\1.jpg", title: "Brighton", alt: "Brighton"
-      
+      src: "..\\images\\brighton\\1.jpg",
+      title: "Brighton",
+      alt: "Brighton",
     },
     {
-      src: "..\\images\\Protest\\1.jpg", title: "Protest", alt: "Protest"
+      src: "..\\images\\Protest\\1.jpg",
+      title: "Protest",
+      alt: "Protest",
     },
     {
-      src: "..\\images\\Grayscale\\1.jpg", title: "Grayscale", alt: "Grayscale"
-      
+      src: "..\\images\\Grayscale\\1.jpg",
+      title: "Grayscale",
+      alt: "Grayscale",
     },
     {
-      src: "..\\images\\Brighton Beach\\1.jpg", title: "Brighton Beach", alt: "Brighton Beach"
+      src: "..\\images\\Brighton Beach\\1.jpg",
+      title: "Brighton Beach",
+      alt: "Brighton Beach",
     },
     {
-      src: "..\\images\\Pride\\1.jpg", title: "Pride", alt: "Pride"
-      
+      src: "..\\images\\Pride\\1.jpg",
+      title: "Pride",
+      alt: "Pride",
     },
     {
-      src: "..\\images\\Birds\\1.jpg", title: "Birds", alt: "Birds"
+      src: "..\\images\\Birds\\1.jpg",
+      title: "Birds",
+      alt: "Birds",
     },
   ];
 
@@ -34,14 +42,24 @@ export default function Galleries() {
         <Header />
       </div>
       <div className="flex flex-col items-center justify-center px-10 lg:px-20 pb-20 mt-32">
-        <span className="text-5xl font-bold text-center font-old-standard text-black mb-8">Galleries</span>
+        <span className="text-5xl font-bold text-center font-old-standard text-black mb-8">
+          Galleries
+        </span>
 
-      <div className="grid lg:grid-cols-3 gap-8">
+        <div className="grid lg:grid-cols-3 gap-8">
           {imagesArray.map((image, index) => (
             <div key={index} className="flex flex-col gap-4">
               {/* Wrap the image in a Link component to route to the gallery item */}
-              <Link to={`/galleries/${image.title.replace(/\s+/g, "-").toLowerCase()}`}>
-                <img className="w-full h-[200px] md:h-[500px]" src={image.src} alt={image.alt} />
+              <Link
+                to={`/galleries/${image.title
+                  .replace(/\s+/g, "-")
+                  .toLowerCase()}`}
+              >
+                <img
+                  className="w-full h-[200px] md:h-[500px]"
+                  src={image.src}
+                  alt={image.alt}
+                />
               </Link>
               <span className="text-3xl font-old-standard text-center font-bold leading-tight text-black">
                 {image.title}
